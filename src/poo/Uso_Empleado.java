c18975b11cfba646497c2f27fcdad29812f41d3a
Mario <mario.toal.01@gmail.com>
2024-01-10 06:47:34 -0600

    se agregan for

--- src/poo/Uso_Empleado.java
+++ src/poo/Uso_Empleado.java
@@ -7,6 +7,56 @@ public class Uso_Empleado {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		/*Empleado empleado1 = new Empleado("Mario Toriz", 85000, 1990, 12, 17);
+
+		Empleado empleado2 = new Empleado("Pao ana ", 95000, 1991, 11, 11);
+
+		Empleado empleado3 = new Empleado("   ana ", 75000, 1993, 05, 03);
+
+		empleado1.subeSueldo(5);
+
+		empleado2.subeSueldo(5);
+
+		empleado3.subeSueldo(5);
+		
+		System.out.println("Nombre : "+ empleado1.dame_nombre() + " Sueldo:  "
+		+ empleado1.dameSueldo()+"  Fecha de Alta "+ empleado1.dameFechaContrato());*/
+		
+		
+		Empleado[] misEmpleados=new Empleado[3];
+		
+		misEmpleados[0]=new Empleado("Mario Toriz", 15600, 1994, 01,19);
+		misEmpleados[1]=new Empleado("otro", 15600, 1992, 04,11);
+		misEmpleados[2]=new Empleado("otro más " , 15600, 1990, 10,10);
+		
+		/*for(int i=0; i<3; i++) {
+			
+			misEmpleados[i].subeSueldo(5);
+		}*/
+		for(Empleado e: misEmpleados) {
+			
+			e.subeSueldo(5);
+			
+		}
+		
+		/*for(int i=0; i<3; i++) {
+			
+			System.out.println("Nombre: " + misEmpleados[i].dame_nombre()
+					+ " sueldo "+ misEmpleados[i].dameSueldo()
+					+ "Fecha de Alta" + misEmpleados[i].dameFechaContrato());
+			
+		}*/
+		for(Empleado e: misEmpleados) {
+			System.out.println("Nombre: " + e.dame_nombre()
+					+ " sueldo "+ e.dameSueldo()
+					+ "Fecha de Alta" + e.dameFechaContrato());
+			
+		}
+			
+		
+		
+		
+
 	}
 
 }
@@ -18,17 +68,33 @@ class Empleado {
 		nombre = nom;
 
 		sueldo = sue;
-		
-		GregorianCalendar calendario=new GregorianCalendar(agno, mes-1,dia);
-		
-		altaContrato =  calendario.getTime();
+
+		GregorianCalendar calendario = new GregorianCalendar(agno, mes - 1, dia);
+
+		altaContrato = calendario.getTime();
 
 	}
-	
-	
+
 	public String dame_nombre() {
-		
+
 		return nombre;
+	}
+
+	public double dameSueldo() {
+
+		return sueldo;
+	}
+
+	public Date dameFechaContrato() {
+
+		return altaContrato;
+	}
+
+	public void subeSueldo(double porcentaje) {
+
+		double aumento = sueldo * porcentaje / 100;
+
+		sueldo += aumento;
 	}
 
 	private String nombre;
