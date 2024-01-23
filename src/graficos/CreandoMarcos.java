a4e165c1ad8644bb0c70e9fa0c1d8e3de97c6b68
Mario <mario.toal.01@gmail.com>
2024-01-23 09:13:32 -0600

    Se manipula ventana

--- src/graficos/CreandoMarcos.java
+++ src/graficos/CreandoMarcos.java
@@ -1,12 +1,43 @@
 package graficos;
 
+import java.awt.Frame;
+
+import javax.swing.*;
+
 public class CreandoMarcos {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		miMarco marco1=new miMarco();
+		
+		marco1.setVisible(true);
+		
+		marco1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 	}
 
+}
+
+class miMarco extends JFrame{
+	
+	public miMarco() {
+		
+		//setSize(500, 300);
+		
+		//setLocation(500,300);
+		
+		
+		setBounds(500,300,550,250);
+		
+		//setResizable(false);
+		
+		//setExtendedState(Frame.MAXIMIZED_BOTH);
+		
+		setTitle("Mi ventana");
+		
+		
+	}
 }
 
 
